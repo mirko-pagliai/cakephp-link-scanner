a94be0a2f95b3336d59ebb7ad5de76f65b57e1b4
mirko-pagliai <mirko.pagliai@gmail.com>
2018-02-21 15:33:18 +0100

    added `LinkScannerShell` class with the `scan()` method and tests

---
+++ src/Shell/LinkScannerShell.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * This file is part of cakephp-link-scanner.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/cakephp-link-scanner
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace LinkScanner\Shell;
+
+use Cake\Console\ConsoleIo;
+use Cake\Console\Shell;
+use Exception;
+use LinkScanner\Event\EventListenerForLinkScannerShell;
+use LinkScanner\Utility\LinkScanner;
+
+/**
+ * A link scanner shell
+ */
+class LinkScannerShell extends Shell
+{
+    /**
+     * A `LinkScanner` instance
+     * @var LinkScanner\Utility\LinkScanner
+     */
+    public $LinkScanner;
+
+    /**
+     * Construct
+     * @param ConsoleIo|null $io A `ConsoleIo` instance
+     * @uses $LinkScanner
+     */
+    public function __construct(ConsoleIo $io = null)
+    {
+        parent::__construct($io);
+
+        $this->LinkScanner = new LinkScanner;
+    }
+
+    /**
+     * Performs a complete scan
+     * @return void
+     * @see EventListenerForLinkScannerShell::implementedEvents()
+     * @uses LinkScanner
+     */
+    public function scan()
+    {
+        try {
+            if ($this->param('fullBaseUrl')) {
+                $this->LinkScanner->setFullBaseUrl($this->param('fullBaseUrl'));
+            }
+            if ($this->param('depth')) {
+                $this->LinkScanner->setMaxDepth($this->param('depth'));
+            }
+            if ($this->param('timeout')) {
+                $this->LinkScanner->setTimeout($this->param('timeout'));
+            }
+
+            //This method will trigger events provided by `EventListenerForLinkScannerShell`
+            $this->LinkScanner->getEventManager()->on(new EventListenerForLinkScannerShell($this));
+
+            $this->LinkScanner->scan();
+        } catch (Exception $e) {
+            $this->abort($e->getMessage());
+        }
+    }
+
+    /**
+     * Gets the option parser instance and configures it
+     * @return ConsoleOptionParser
+     */
+    public function getOptionParser()
+    {
+        $parser = parent::getOptionParser();
+
+        $parser->setDescription(__d('database_backup', 'Shell to perform links scanner'));
+
+        $parser->addSubcommand('scan', [
+            'help' => __d('link-scanner', 'Performs a complete scan'),
+            'parser' => [
+                'options' => [
+                    'depth' => [
+                        'help' => __d('link-scanner', 'Maximum depth of the scan. Default: {0}', $this->LinkScanner->maxDepth),
+                        'short' => 'd',
+                    ],
+                    'fullBaseUrl' => [
+                        'help' => __d('link-scanner', 'Full base url. By default, the `{0}` value will be used', 'App.fullBaseUrl'),
+                        'short' => 'f',
+                    ],
+                    'timeout' => [
+                        'help' => __d('link-scanner', 'Timeout in seconds for each request. Default: {0}', $this->LinkScanner->timeout),
+                        'short' => 't',
+                    ],
+                ],
+            ],
+        ]);
+
+        return $parser;
+    }
+}
---
+++ tests/TestCase/Shell/LinkScannerShellTest.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * This file is part of cakephp-link-scanner.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/cakephp-link-scanner
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace LinkScanner\Test\TestCase\Shell;
+
+use Cake\Console\ConsoleIo;
+use Cake\TestSuite\ConsoleIntegrationTestCase;
+use Cake\TestSuite\Stub\ConsoleOutput;
+use LinkScanner\Shell\LinkScannerShell;
+use LinkScanner\Utility\LinkScanner;
+use Reflection\ReflectionTrait;
+use Zend\Diactoros\Stream;
+
+/**
+ * LinkScannerShellTest class
+ */
+class LinkScannerShellTest extends ConsoleIntegrationTestCase
+{
+    use ReflectionTrait;
+
+    /**
+     * @var \LinkScanner\Shell\LinkScannerShell;
+     */
+    protected $LinkScannerShell;
+
+    /**
+     * @var \Cake\TestSuite\Stub\ConsoleOutput
+     */
+    protected $err;
+
+    /**
+     * @var \Cake\TestSuite\Stub\ConsoleOutput
+     */
+    protected $out;
+
+    /**
+     * Internal method to get a `LinkScanner` instance with a stub for the
+     *  `Client` instance.
+     * @return LinkScanner
+     */
+    protected function getLinkScannerWithStubClient()
+    {
+        $LinkScanner = new LinkScanner('http://google.com');
+
+        $LinkScanner->Client = $this->getMockBuilder(get_class($LinkScanner->Client))
+            ->setMethods(['get'])
+            ->getMock();
+
+        $LinkScanner->Client->method('get')
+            ->will($this->returnCallback(function () {
+                $request = unserialize(file_get_contents(TESTS . 'response_examples' . DS . 'google_response'));
+                $body = unserialize(file_get_contents(TESTS . 'response_examples' . DS . 'google_body'));
+                $stream = new Stream('php://memory', 'rw');
+                $stream->write($body);
+                $this->setProperty($request, 'stream', $stream);
+
+                return $request;
+            }));
+
+        return $LinkScanner;
+    }
+
+    /**
+     * Setup the test case, backup the static object values so they can be
+     * restored. Specifically backs up the contents of Configure and paths in
+     *  App if they have not already been backed up
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->out = new ConsoleOutput;
+        $this->err = new ConsoleOutput;
+        $io = new ConsoleIo($this->out, $this->err);
+        $io->level(2);
+
+        $this->LinkScannerShell = $this->getMockBuilder(LinkScannerShell::class)
+            ->setMethods(['in', '_stop'])
+            ->setConstructorArgs([$io])
+            ->getMock();
+
+        $this->LinkScannerShell->LinkScanner = $this->getLinkScannerWithStubClient();
+    }
+
+    /**
+     * Test for `scan()` method
+     * @test
+     */
+    public function testScan()
+    {
+        $this->LinkScannerShell->scan();
+        $messages = $this->out->messages();
+        $this->assertCount(3, $messages);
+        $this->assertRegexp('/^Scan started for ' . preg_quote('http://google.com/', '/') . ' at [\d\-]+\s[\d\:]+$/', current($messages));
+        $this->assertRegexp('/^Scan completed at [\d\-]+\s[\d\:]+$/', next($messages));
+        $this->assertRegexp('/^Total scanned links: \d+$/', next($messages));
+        $this->assertEmpty($this->err->messages());
+    }
+
+    /**
+     * Test for `scan()` method, with verbose
+     * @test
+     */
+    public function testScanVerbose()
+    {
+        $this->LinkScannerShell->params['verbose'] = true;
+        $this->LinkScannerShell->scan();
+        $messages = $this->out->messages();
+        $count = count($messages);
+
+        //First three lines
+        $this->assertRegexp('/^\-{63}$/', $messages[0]);
+        $this->assertRegexp('/^Scan started for ' . preg_quote('http://google.com/', '/') . ' at [\d\-]+\s[\d\:]+$/', $messages[1]);
+        $this->assertRegexp('/^\-{63}$/', $messages[2]);
+
+        //Last four lines
+        $this->assertRegexp('/^\-{63}$/', $messages[$count - 1]);
+        $this->assertRegexp('/^Total scanned links: \d+$/', $messages[$count - 2]);
+        $this->assertRegexp('/^Scan completed at [\d\-]+\s[\d\:]+$/', $messages[$count - 3]);
+        $this->assertRegexp('/^\-{63}$/', $messages[$count - 4]);
+
+        //Removes the already checked lines
+        foreach ([0, 1, 2, $count - 1, $count - 2, $count - 3, $count - 4] as $line) {
+            unset($messages[$line]);
+        }
+
+        $messages = array_values($messages);
+        $count = count($messages);
+
+        //Checks the intermediate lines
+        for ($line = 0; $line < $count;) {
+            $this->assertTextStartsWith('Checking ', $messages[$line]);
+            $this->assertTextEndsWith(' ...', $messages[$line]);
+            $line++;
+            $this->assertEquals('<success>OK</success>', $messages[$line++]);
+            $this->assertRegexp('/^Links found: \d+$/', $messages[$line++]);
+        }
+    }
+
+    /**
+     * Test for `getOptionParser()` method
+     * @test
+     */
+    public function testGetOptionParser()
+    {
+        $parser = $this->LinkScannerShell->getOptionParser();
+
+        $this->assertInstanceOf('Cake\Console\ConsoleOptionParser', $parser);
+        $this->assertEquals([
+            'scan'
+        ], array_keys($parser->subcommands()));
+
+        $this->assertEquals('Shell to perform links scanner', $parser->getDescription());
+        $this->assertEquals(['help', 'quiet', 'verbose'], array_keys($parser->options()));
+
+        //`scan` subcommand
+        $scanSubcommandParser = $parser->subcommands()['scan']->parser();
+        $this->assertEquals([
+            'depth',
+            'fullBaseUrl',
+            'help',
+            'quiet',
+            'timeout',
+            'verbose',
+        ], array_keys($scanSubcommandParser->options()));
+    }
+}
