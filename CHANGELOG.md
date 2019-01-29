58f406648016ea613a5a35dc1964b314c7a41bb5
mirko-pagliai <mirko.pagliai@gmail.com>
2019-01-29 14:19:50 +0100

    added API

--- CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# 1.x branch
-### 1.0.1-beta
-* second release.
-
-### 1.0.0-beta
-* first release.
---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","LinkScanner\\Command\\LinkScannerCommand"],["c","LinkScanner\\Event\\LinkScannerCommandEventListener"],["c","LinkScanner\\Event\\LinkScannerEventListenerInterface"],["c","LinkScanner\\Http\\Client\\ScanResponse"],["c","LinkScanner\\ORM\\ScanEntity"],["c","LinkScanner\\Plugin"],["c","LinkScanner\\ResultScan"],["c","LinkScanner\\TestSuite\\IntegrationTestTrait"],["c","LinkScanner\\TestSuite\\TestCase"],["c","LinkScanner\\Utility\\LinkScanner"]];
