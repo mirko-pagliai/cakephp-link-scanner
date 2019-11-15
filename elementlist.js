4f03e6708f16e8e153ddb0f553ddfb579d115631
mirko-pagliai <mirko.pagliai@gmail.com>
2019-11-15 14:34:07 +0100

    updated

--- elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","LinkScanner\\Command\\LinkScannerCommand"],["c","LinkScanner\\Event\\LinkScannerCommandEventListener"],["c","LinkScanner\\Event\\LinkScannerEventListenerInterface"],["c","LinkScanner\\Plugin"],["c","LinkScanner\\ResultScan"],["c","LinkScanner\\ScanEntity"],["c","LinkScanner\\TestSuite\\IntegrationTestTrait"],["c","LinkScanner\\TestSuite\\TestCase"],["c","LinkScanner\\Utility\\LinkScanner"]];
