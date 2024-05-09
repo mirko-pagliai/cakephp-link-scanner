e62a175e6089942eaf275b3ae070372d05470910
mirko-pagliai <mirko.pagliai@gmail.com>
2024-05-09 16:11:10 +0200

    added (empty) global_functions.php file

---
+++ src/global_functions.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of cakephp-link-scanner.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/cakephp-link-scanner
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
